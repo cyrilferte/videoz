98f7aa18a784c05dc523d0c29539c08299d4671a
cyril ferte <cyril@fertec.fr>
2016-12-26 20:49:31 +0000

    first heroku push3

--- app/assets/javascripts/application.js
+++ app/assets/javascripts/application.js
@@ -1,6 +1,4 @@
 //= require jquery
 //= require jquery_ujs
 //= require bootstrap-sprockets
-//= require algoperso
-//= require algolia/v3/algoliasearch.min
 //= require_tree .
--- app/models/film.rb
+++ app/models/film.rb
@@ -1,18 +1,4 @@
 class Film < ApplicationRecord
-     include AlgoliaSearch
-
-  algoliasearch do
-    attributes :titre, :date, :style, :tags, :link
     
-    # the attributesToIndex` setting defines the attributes
-    # you want to search in: here `title`, `subtitle` & `description`.
-    # You need to list them by order of importance. `description` is tagged as
-    # `unordered` to avoid taking the position of a match into account in that attribute.
-    attributesToIndex ['titre', 'date', 'style','tags','link']
-
-    # the `customRanking` setting defines the ranking criteria use to compare two matching
-    # records in case their text-relevance is equal. It should reflect your record popularity.
-    customRanking ['desc(date)']
-  end
   
 end
